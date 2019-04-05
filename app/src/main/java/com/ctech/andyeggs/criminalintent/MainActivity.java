924afa7e705613fd857095d38233cd961d22bdba
Andy E <andyjohneggs@gmail.com>
2019-04-05 08:23:11 -0500

    Unit 7 Day 2a, completed assignment, myCrimeFragement implemented in MainActivity, no errors, runs, SHOWS ALL UI, Success!

--- app/src/main/java/com/ctech/andyeggs/criminalintent/MainActivity.java
+++ app/src/main/java/com/ctech/andyeggs/criminalintent/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ctech.andyeggs.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment myCrimeFragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (myCrimeFragment == null) {
+            myCrimeFragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, myCrimeFragment)
+                    .commit();
+        }
     }
 }
