fd2575fcebbbba1cc3068915ac23a24ec0fdbe5f
Andy E <andyjohneggs@gmail.com>
2019-04-15 09:04:28 -0500

    Unit 10 Day 1, Added all of the code needed for this one. One error! Does not run! Need to fix the Serializable to UUID error. FIX! Assignment completed!

--- app/src/main/java/com/ctech/andyeggs/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/ctech/andyeggs/criminalintent/CrimeActivity.java
@@ -1,11 +1,27 @@
 package com.ctech.andyeggs.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
+    private static final String EXTRA_CRIME_ID = "com.ctech.andyeggs.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+        Intent myIntent = new Intent(packageContext, CrimeActivity.class);
+        myIntent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return myIntent;
+    }
+
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        //get the crimeId that was passed to us from the CrimeList via the Intent
+        UUID crimeId = getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        //call the "NewInstance" method which will create a new CrimeFragment and store the crimeId inside
+        return CrimeFragment.newInstance(crimeId);
     }
 }
--- app/src/main/java/com/ctech/andyeggs/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/ctech/andyeggs/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.ctech.andyeggs.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -62,7 +63,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            Intent myIntent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(myIntent);
         }
 
         public void bind(Crime crime) {
