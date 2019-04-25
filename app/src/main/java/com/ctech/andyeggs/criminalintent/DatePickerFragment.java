12685e3121a1d68e4cb865abb63b348958617c22
Andy E <andyjohneggs@gmail.com>
2019-04-25 08:56:52 -0500

    Unit 12 Day 1, not completed, halfway done with assignment (halfway down document), Calendar shows!, Correct Date shown on DatePicker but doesn't change date in crime yet, Added dialog_date.xml, Tested, works, functional. No Errors.

--- app/src/main/java/com/ctech/andyeggs/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/ctech/andyeggs/criminalintent/DatePickerFragment.java
@@ -6,12 +6,48 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "crime_data";
+
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment myFragment = new DatePickerFragment();
+        myFragment.setArguments(args);
+        return myFragment;
+    }
+
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        Date myDate = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar myCalendar = Calendar.getInstance();
+        myCalendar.setTime(myDate);
+        int year = myCalendar.get(Calendar.YEAR);
+        int month = myCalendar.get(Calendar.MONTH);
+        int day = myCalendar.get(Calendar.DAY_OF_MONTH);
+
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
